9022e90ec6dc83895549371b81f9a7b4b696a646
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2019-01-06 12:21:31 -0500

    Working state with more solid authentication

--- populate_settings.js
+++ populate_settings.js
@@ -7,7 +7,7 @@ function setValueToStorage(key, value) {
 }
 
 var getSettingsInfo = function () {
-	let response = chrome.runtime.sendMessage(window.localStorage.getItem('extensionID'), '{extension: "active"}', function (res) {
+	let response = chrome.runtime.sendMessage(window.localStorage.getItem('extensionID'), '{extension: "active", settings_mode: true}', function (res) {
 		var pfntSiteData = JSON.parse(document.getElementById('pfnt__pfSiteData').value);
 		console.log('PFNT Setting Prep', pfntSiteData, document);
 		chrome.storage.local.set(pfntSiteData, function () {
@@ -17,5 +17,5 @@ var getSettingsInfo = function () {
 	});
 
 };
-
+console.log('Settings Population Prep');
 setTimeout(getSettingsInfo, 10000);
