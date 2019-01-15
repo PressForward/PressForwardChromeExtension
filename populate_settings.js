da72875cf5009d4634dafc287717c5618da1fe16
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2019-01-14 21:33:26 -0500

    Setting up pick up of key values for signing Nominations from the extension

--- populate_settings.js
+++ populate_settings.js
@@ -6,16 +6,49 @@ function setValueToStorage(key, value) {
 	});
 }
 
-var getSettingsInfo = function () {
-	let response = chrome.runtime.sendMessage(window.localStorage.getItem('extensionID'), '{extension: "active", settings_mode: true}', function (res) {
+var getSettingsInfo = function (button) {
+	let self = {};
+	self.button = button;
+	let response = chrome.runtime.sendMessage(window.localStorage.getItem('extensionID'), '{extension: "active", settings_mode: true}', (res) => {
 		var pfntSiteData = JSON.parse(document.getElementById('pfnt__pfSiteData').value);
 		console.log('PFNT Setting Prep', pfntSiteData, document);
-		chrome.storage.local.set(pfntSiteData, function () {
+		chrome.storage.local.set(pfntSiteData, () => {
 			console.log("System variables set for PFNT");
+			if (self.button) {
+				console.log('button', self.button);
+				var button = document.getElementById("pressforward-nt__" + self.button);
+				button.style.backgroundColor = 'lightgreen';
+				button.innerHTML = 'API Keys Saved';
+			}
 		});
 		console.log(res);
 	});
 
 };
-console.log('Settings Population Prep');
+console.log('PFNT Settings Population Prep');
+
+
+console.log('PFNT Settings Population Hooking');
 setTimeout(getSettingsInfo, 10000);
+
+console.log('PFNT bind_populate_settings.js');
+window.setTimeout(function () {
+	console.log('PFNT timeout completed, binding');
+	var setupButton = document.getElementById("pressforward-nt__setup-button")
+	if (setupButton) {
+		setupButton.addEventListener("click", (e) => {
+			//document.addEventListener("click", (e) => {
+			getSettingsInfo('setup-button');
+
+		});
+	}
+	var regenButton = document.getElementById("pressforward-nt__regenerate-button")
+	if (regenButton) {
+		regenButton.addEventListener("click", (e) => {
+			//document.addEventListener("click", (e) => {
+			window.setTimeout(function () {
+				getSettingsInfo('regenerate-button');
+			}, 5000);
+		});
+	}
+}, 5000);
--- settings.js
+++ settings.js
@@ -23,8 +23,8 @@ chrome.storage.local.get(['submit_endpoint'], function (result) {
 	console.log('submit_endpoint Value currently is ' + result);
 	document.getElementById('pfSiteData_submit_endpoint').value = result.submit_endpoint;
 });
-window.pfSiteData = {};
-window.pfSiteData.site_url = "https://local.wordpress.test";
-window.pfSiteData.submit_endpoint = "https://local.wordpress.test/wp-json\/pf/v1/submit-nomination";
-window.pfSiteData.categories_endpoint = 'https://local.wordpress.test/wp-json/wp/v2/categories';
-window.pfSiteData.userkey = '65794a30655841694f694a4b563151694c434a68624763694f694a49557a49314e694a392e65794a725a586c666332566c5a434936496e426d4969776964584e6c636c397a5a57566b496a6f69597a49344d3246694e44526a4e7a4e68596d5532593245784d54416966512e434e36667835645a4744425f3367497145334936316847643547702d634f644c494a6770776a6a30514267';
+// window.pfSiteData = {};
+// window.pfSiteData.site_url = "https://local.wordpress.test";
+// window.pfSiteData.submit_endpoint = "https://local.wordpress.test/wp-json\/pf/v1/submit-nomination";
+// window.pfSiteData.categories_endpoint = 'https://local.wordpress.test/wp-json/wp/v2/categories';
+// window.pfSiteData.userkey = '65794a30655841694f694a4b563151694c434a68624763694f694a49557a49314e694a392e65794a725a586c666332566c5a434936496e426d4969776964584e6c636c397a5a57566b496a6f69597a49344d3246694e44526a4e7a4e68596d5532593245784d54416966512e434e36667835645a4744425f3367497145334936316847643547702d634f644c494a6770776a6a30514267';
